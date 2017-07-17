758746e6d8dd2c24921b9356bf89fcddb0dad86c
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-07-17 17:19:18 -0500

    question posting working/answer

--- trivia-game/trivia.js
+++ trivia-game/trivia.js
@@ -1,79 +1,5 @@
 var triviaGame = {
 
-    questions: {
-
-        q1: {
-            question: "Which herb comes from the dried stigmas of crocus flowers?",
-            correctAnswer: "Saffron",
-            wrongAnswer1: "Turmeric",
-            wrongAnswer2: "Fennel",
-            wrongAnswer3: "Wasabi"
-        },
-        q2: {
-            question: "What musical term describes a palate-cleansing course served between two larger courses?",
-            correctAnswer: "Intermezzo",
-            wrongAnswer1: "Fortissimo",
-            wrongAnswer2: "Pianissimo",
-            wrongAnswer3: "Allegrito"
-        },
-        q3: {
-            question: "Which type of pastry is used to make profiteroles?",
-            correctAnswer: "Pâte à choux",
-            wrongAnswer1: "Puff Pastry",
-            wrongAnswer2: "Filo Dough",
-            wrongAnswer3: "Tart Dough"
-        },
-        q4: {
-            question: "Which of the following is NOT a blue cheese?",
-            correctAnswer: "Camembert",
-            wrongAnswer1: "Roquefort",
-            wrongAnswer2: "Stilton",
-            wrongAnswer3: "Gorgonzola"
-        },
-        q5: {
-            question: "What is the more common, American name, for Mille-feuille",
-            correctAnswer: "Napoleon",
-            wrongAnswer1: "Sacher",
-            wrongAnswer2: "Macaron",
-            wrongAnswer3: "Éclair"
-        },
-        q6: {
-            question: "What does the Italian 'al dente' literally translate as?",
-            correctAnswer: "To the tooth",
-            wrongAnswer1: "To the taste",
-            wrongAnswer2: "Slightly firm",
-            wrongAnswer3: "To the mouth"
-        },
-        q7: {
-            question: "Which of these is NOT a type of mushroom?",
-            correctAnswer: "Eel",
-            wrongAnswer1: "Morel",
-            wrongAnswer2: "Oyster",
-            wrongAnswer3: "Chanterelle"
-        },
-        q8: {
-            question: "What usually makes yellow curry yellow?",
-            correctAnswer: "Turmeric",
-            wrongAnswer1: "Mustard",
-            wrongAnswer2: "Ginger",
-            wrongAnswer3: "Saffron"
-        },
-        q9: {
-            question: "Which of these countries consumes the most sheep/lamb per capita?",
-            correctAnswer: "Sudan",
-            wrongAnswer1: "United States",
-            wrongAnswer2: "Australia",
-            wrongAnswer3: "New Zealand"
-        },
-        q10: {
-            question: "Which of these countries consumes the most milk per capita?",
-            correctAnswer: "Finland",
-            wrongAnswer1: "Switzerland",
-            wrongAnswer2: "Albania",
-            wrongAnswer3: "United States"
-        }
-    },
-
     init: function() {
         this.initialDom();
 
@@ -85,12 +11,11 @@ var triviaGame = {
         var wrong = 0;
         var missed = 0;
 
-
         //event listener
         var clicked = "";
         $("body").on("click", ".btn", function() {
             clicked = $(this).attr("id");
-            triviaGame.handleClicks(clicked);
+            triviaGame.handleClicks(clicked, questionCount);
         });
 
         //timer
@@ -106,23 +31,35 @@ var triviaGame = {
         $("#no-btn").append("<button type='button' id='no' class='btn btn-danger'>No</button>");
     },
 
-    writeQandA: function() {
+    writeQandA: function(questionCount) {
+    	var n = questionCount;
         $(".answer-container").show();
         $("#timer-holder").show();
         $("#stats").show();
 
-        for (var i = 1; i < 11; i++) {
-            var qn = "q" + i;
-            $("#questions").text(qn);
+        if (questionCount < questions.length) {
+
+            $('#questions').text(questions[n].question);
+
+            for (var i = 0; i < questions[n].question.length; i++) {
+                $("#answer1").text(questions[n].answers[0]);
+                $("#answer2").text(questions[n].answers[1]);
+                $("#answer3").text(questions[n].answers[2]);
+                $("#answer4").text(questions[n].answers[3]);
+            }
+
+            
+        } else {
+            resetGame();
         }
     },
 
-    handleClicks: function(getClicked) {
+    handleClicks: function(getClicked, questionCount) {
 
         if (getClicked === "yes") {
             $("#yes-btn").hide();
             $("#no-btn").hide();
-            this.writeQandA();
+            this.writeQandA(questionCount);
 
         }
         if (getClicked === "no") {
@@ -149,10 +86,61 @@ var triviaGame = {
     },
 
     timesUp: function() {
-    	setTimeout(wait, 4000);  //  change to 4000 or other amount
+        setTimeout(wait, 4000); //  change to 4000 or other amount
 
     }
 
 }
+
+var questions = [
+
+    {
+        "question": "Which herb comes from the dried stigmas of crocus flowers?",
+        "answers": ["Saffron", "Turmeric", "Fennel", "Wasabi"],
+        "correctA": 0
+
+    }, {
+        question: "What musical term describes a palate-cleansing course served between two larger courses?",
+        answers: ["Fortissimo", "Allegrito", "Intermezzo", "Pianissimo"],
+        correctA: 2
+    }, {
+        question: "Which type of pastry is used to make profiteroles?",
+        answers: ["Filo Dough", "Pâte à choux", "Puff Pastry", "Tart Dough"],
+        correctA: 1
+
+    }, {
+        question: "Which of the following is NOT a blue cheese?",
+        answers: ["Stilton", "Gorgonzola", "Camembert", "Roquefort"],
+        correctA: 2
+
+    }, {
+        question: "What is the more common, American name, for Mille-feuille",
+        answers: ["Éclair", "Macaron", "Sacher", "Napoleon"],
+        correctA: 3
+
+    }, {
+        question: "What does the Italian 'al dente' literally translate as?",
+        answers: ["To the taste", "To the tooth", "Slightly firm", "To the mouth"],
+        correctA: 1
+    }, {
+        question: "Which of these is NOT a type of mushroom?",
+        answers: ["Eel", "Morel", "Oyster", "Chanterelle"],
+        correctA: 0
+    }, {
+        question: "What usually makes yellow curry yellow, and red curry red, respectively?",
+        answers: ["Turmeric & Paprika", "Ginger & Beets", "Mustard & Red Peppers", "Saffron & Paprika"],
+        correctA: 0
+    }, {
+        question: "Which of these countries consumes the most sheep/lamb per capita?",
+        answers: ["United States", "Australia", "New Zealand", "Sudan", ],
+        correctA: 3
+    }, {
+        question: "Which of these countries consumes the most milk per capita?",
+        answers: ["Albania", "Finland", "Switzerland", "United States"],
+        correctA: 1
+    }
+
+
+]
 
 triviaGame.init();
