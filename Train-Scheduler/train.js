5ee10b30cb1e2873bf2e2de8ea6e7e897a6953ae
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-07-31 11:40:40 -0500

    train hw working and meets assignment criteria

--- Train-Scheduler/train.js
+++ Train-Scheduler/train.js
@@ -8,14 +8,18 @@ var trainScheduler = {
         var trainFrequency = "";
         var trainTime = "";
         var submitCounter = 0;
+        var timeDiff = 0;
+        var trainHr = "";
+        var trainMin = "";
         firebase.initializeApp(config);
         var database = firebase.database();
-        this.renderDom();
+        this.renderDom(database);
         this.handleClicks(submitCounter, database);
     },
-    renderDom: function() {
+    renderDom: function(database) {
         var timeHrSel = $("#train-time-hr");
-
+        var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        //$("#current-time").text("Current time: " + currentTime)
         for (var i = 0; i < 24; i++) {
             if (i < 10) {
                 $(timeHrSel).append("<option id='hr-0" + [i] + "'>" + "0" + [i]);
@@ -23,30 +27,69 @@ var trainScheduler = {
                 $(timeHrSel).append("<option id='hr-" + [i] + "'>" + [i]);
             }
         }
-        for (var j = 0; j < 10; j++) {
+        for (var j = 0; j < 6; j++) {
             $("#train-time-min1").append("<option id='min1-" + [j] + "'>" + [j]);
         }
         for (var jj = 0; jj < 10; jj++) {
             $("#train-time-min2").append("<option id='min2-" + [jj] + "'>" + [jj]);
         }
+
+        database.ref().on("child_added", function(data) {
+
+            trainHr = data.val().timeHr;
+            trainMin = data.val().timeMin;
+            trainTime = trainHr + ":" + trainMin;
+            trainFrequency = data.val().frequency;
+
+            // First Time (pushed back 1 year to make sure it comes before current time)
+            var firstTimeConverted = moment(trainTime, "hh:mm").subtract(1, "years");
+            console.log(firstTimeConverted);
+            // Current Time
+            var currentTime = moment();
+            console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+            // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log("DIFFERENCE IN TIME: " + diffTime);
+
+            // Time apart (remainder)
+            var tRemainder = diffTime % trainFrequency;
+            console.log(tRemainder);
+
+            // Minute Until Train
+            var tMinutesTillTrain = trainFrequency - tRemainder;
+            console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+            // Next Train
+            var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+            nextTrain = moment(nextTrain).format("hh:mm");
+            console.log("ARRIVAL TIME: " + nextTrain);
+
+            $("#example-1").append("<tr><td>" + data.val().name + "</td><td>" + data.val().destination + "</td><td>" + data.val().frequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td>");
+        })
     },
     handleClicks: function(submitCounter, database) {
         $("#submit").on("click", function() {
 
             event.preventDefault();
-           
+
             trainName = $("#train-name").val().trim();
             trainDestination = $("#destination").val().trim();
             trainTimeHr = $("#train-time-hr").val().trim();
             trainTimeMin1 = $("#train-time-min1").val().trim();
-            trainTimeMin2 = $("#train-time-min1").val().trim();
+            trainTimeMin2 = $("#train-time-min2").val().trim();
             trainFrequency = $("#frequency").val().trim();
-            trainTime = trainTimeHr + ":" + trainTimeMin1 + trainTimeMin2;
+            trainHr = parseInt(trainTimeHr);
+            trainMin = trainTimeMin1 + trainTimeMin2;
+            trainMin = parseInt(trainMin);
 
-            console.log(trainName, trainDestination, trainTime, trainFrequency)
+            database.ref().push({
 
-            database.ref().set({
-                Train1: [trainName, trainDestination, trainTime, trainFrequency]
+                name: trainName,
+                destination: trainDestination,
+                timeHr: trainHr,
+                timeMin: trainMin,
+                frequency: trainFrequency
             });
             trainScheduler.appendDom(database);
 
@@ -54,12 +97,12 @@ var trainScheduler = {
 
     },
     appendDom: function(database) {
-        database.ref().on("value", function(snapshot) {
+        database.ref().on("value", function(data) {
 
-            $("#highest-bidder").html(snapshot.val().highBidder);
-            $("#highest-price").html(snapshot.val().highPrice);
+            //$("#highest-bidder").html(data.val().highBidder);
+            //$("#highest-price").html(data.val().highPrice);
 
-            $("#example-1").after("<tr><td>"+snapshot.val().Train1[0]+"</td><td>"+snapshot.val().Train1[1]+"</td><td>"+snapshot.val().Train1[3]+"</td>")
+            //$("#example-1").after("<tr><td>"+data.val().Train1[0]+"</td><td>"+data.val().Train1[1]+"</td><td>"+data.val().Train1[3]+"</td>")
         })
     }
 
