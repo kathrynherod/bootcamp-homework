9d8dbfa571f3920e29ed7c324ce362a47a179d47
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-09-21 23:02:18 -0600

    typos

--- Week-13_Friend-Finder/server.js
+++ Week-13_Friend-Finder/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + 'app/public'));
 //app.use(express.static(__dirname + '/../public/style.css'));
 
 //require the routes
