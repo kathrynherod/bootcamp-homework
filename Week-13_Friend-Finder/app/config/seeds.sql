77016b57a54a02e96b08bc25a2e4c4e00289e0ba
Emma Pariskathrynherod <codingkat7@gmail.com>
2017-09-27 19:51:33 -0500

    it's pretty

---
+++ Week-13_Friend-Finder/app/config/seeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Charles McDowell','https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/14224967_358092211189516_532114160343420505_n.jpg?oh=0aaf2d8540b74d31423577d05f422fc4&oe=5A4AAC19',1,5,1,1,1,1,5,5,1,5);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Cam Kirkland','https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAL2AAAAJGE3MDU0NmQwLWMyZTktNGYzMi1iNGM1LTc1MmY3Mjg3ZmVkYg.jpg',5,1,2,4,4,2,4,4,3,4);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Yogashri Pradhan','https://uthoustonbootcamp0617.slack.com/team/U5KJKJJQH',5,5,3,5,5,4,5,5,5,4);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Michelle Bury','https://uthoustonbootcamp0617.slack.com/team/U5AAYCYJY',1,4,3,5,2,1,4,2,2,2);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Art Tregubchak','https://uthoustonbootcamp0617.slack.com/team/U5KFQN7R6',3,5,3,4,3,1,4,5,2,4);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Yahya Al zubaidi','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png',2,3,2,2,2,2,4,3,3,2);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES ('Rachel Estrada','https://files.slack.com/files-pri/T5AAYCYF6-F7A1KKD53/rachel.jpg',4,5,4,5,4,5,5,4,5,5);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES("Jakie Rice","https://uthoustonbootcamp0617.slack.com/team/U5L1X0JG1",1,5,2,5,1,4,5,3,3,3);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES("Sonia Bradley","https://uthoustonbootcamp0617.slack.com/team/U5LSGRDAT",3,3,3,4,3,4,5,4,5,3);
+
+INSERT INTO friend_list 
+(friend_name, friend_photo_url,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10) 
+VALUES("Jeremy Jackson","https://uthoustonbootcamp0617.slack.com/team/U5L06L7GV",3, 3, 2, 4, 2, 3, 4, 4, 5, 5);
+
